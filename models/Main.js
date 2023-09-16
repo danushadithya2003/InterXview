ad28043d9a57db2c9d88296ce3a4c9e9be8e7f1c
sanjay <sanjay.rahul@conversight.ai>
2023-09-16 12:57:21 +0530

    form changes

--- models/Main.js
+++ models/Main.js
@@ -16,15 +16,26 @@ const userSchema = new mongoose.Schema({
 
 // Interview experience document schema
 const experienceSchema = new mongoose.Schema({
-    title: String,
-    content: String,
-    feedback: String,
+    company: String,    
+    position: String,
+    feedback: String, 
+    content: String,  
     date: String,
     user: {
         type: mongoose.Schema.Types.ObjectId,
-        ref: "User" // Reference to the User model
+        ref: "User"
     }
 });
+// const experienceSchema = new mongoose.Schema({
+//     title: String,
+//     content: String,
+//     feedback: String,
+//     date: String,
+//     user: {
+//         type: mongoose.Schema.Types.ObjectId,
+//         ref: "User" // Reference to the User model
+//     }
+// });
 
 userSchema.pre("save", async function (next) {
     if (!this.isModified("password")) {
